1d428ecdd6bf8d2ee144bff16ee97e9addb8edef
Vinícius Araújo <134007470+v1985araujo@users.noreply.github.com>
2023-07-27 16:39:01 -0300

    Uma outra forma de resolver o problema
    Foi feita uma conversão de string para lista, alterando a letra e depois passando de volta para string

--- palavra.py
+++ palavra.py
@@ -1,34 +1,31 @@
-def manipular_palavra(palavra:str, letra:str, posicao:int) -> str:
+def manipular(palavra:str, posicao:int, letra:str) -> str:
     """
-    Esse função vai trocar a letra de uma plalavra em uma dada posição, começando de 1
+    Esse função vai trocar a letra de uma palavra em uma dada posição, começando de 1
     :param letra: uma ou mais letras a ser(em) colocada(s) na palavra
     :param posicao: a posição da letra a ser alterada
     :param palavra: a string a ser mexida
     :return: a palavra com a alteração feita
     """
-    string = palavra[:int(posicao) - 1]
-    string += letra
-    string += palavra[int(posicao):]
+    lista = list(palavra)
+    lista[posicao - 1] = letra
+    string = str(lista)
+    string = string.replace("'", "")
+    string = string.replace(",", "")
+    string = string.replace("[", "")
+    string = string.replace("]", "")
+    string = string.replace(" ", "")
 
     return string
 
-# A maior palavra em língua portuguesa: pneumoultramicroscopicossilicovulcanoconiótico
-num = [str(x) for x in range(1, 47)]
 
-pos = 0
 palavra = input("Digite uma palavra: ")
 letra = input(f"Qual a letra que vai entrar na {palavra}? ")
-
+num = [str(x) for x in range(1, len(palavra) + 1)]
+pos = 0
 while pos not in num:
-    pos = input(f"Qual a posição da {palavra} que vai ser alterada? ")
+    pos = input(f"A posição de {palavra} que vai ser alterada? ")
     if pos not in num:
         print(f"Esse dado precisa estar entre 1 e {len(palavra)}")
 
 
-if(int(pos) <= len(palavra)):
-    string = manipular_palavra(palavra, letra, int(pos))
-
-else:
-    string = f"A posição {pos} não existe em {palavra}!"
-
-print(string)
+print(manipular(palavra, int(pos), letra))
